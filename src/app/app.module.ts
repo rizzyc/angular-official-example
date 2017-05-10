ff20731b582a131a0639224a69463716aa5428f9
Rizwan <rizwan@riz-e-designs.com>
2017-05-10 14:28:23 -0400

    http module However details panels not working goes into infinite loop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
+import { HeroSearchComponent } from "./hero-search.component";
 
 import { AppRoutingModule } from './app-routing-module';
 
-
-
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [
     HeroService
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -22,16 +22,36 @@ export class HeroesComponent implements OnInit {
      private router: Router,
      private heroService: HeroService) {}
 
-   getHeroesSlowly(): void {
+   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
    }
 
    ngOnInit(): void{
-     this.getHeroesSlowly();
+     this.getHeroes();
    }
 
    gotoDetail(): void {
      this.router.navigate(['/detail', this.selectedHero.id]);
    }
+
+   add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+delete(hero: Hero): void {
+  this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
+}
+
 }
 
